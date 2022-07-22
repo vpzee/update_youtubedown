7db1a6ad12af3da454e13c2da6d50e5b22fdbb10
Vincent Zee <vpzee@protonmail.com>
2022-07-22 22:49:53 +0200

    Initial commit of this terrible hack'

---
+++ get_youtubedown.v
@@ -0,0 +1,10 @@
+import net.http
+import os
+
+const (
+		home_dir = os.home_dir()
+		tempfile = home_dir + "/bin/tempfile"
+		url = "https://jwz.org/hacks/youtubedown"
+)
+
+http.download_file(url, tempfile)?
