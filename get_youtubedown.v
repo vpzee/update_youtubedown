a0890c011bf08fdde567296766823e1982265571
Vincent Zee <vpzee@protonmail.com>
2023-02-21 05:42:37 +0100

    add get_youtubedown executable

--- get_youtubedown.v
+++ get_youtubedown.v
@@ -2,9 +2,9 @@ import net.http
 import os
 
 const (
-		home_dir = os.home_dir()
-		tempfile = home_dir + "/bin/tempfile"
-		url = "https://jwz.org/hacks/youtubedown"
+	home_dir = os.home_dir()
+	tempfile = home_dir + "/bin/tempfile"
+	url = "https://jwz.org/hacks/youtubedown"
 )
 
-http.download_file(url, tempfile)?
+http.download_file(url, tempfile)!
